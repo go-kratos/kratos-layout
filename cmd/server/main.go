bf8646ea88d878e8200f6f6b2e26b8096462b73c
chenzhihui <zhihui_chen@foxmail.com>
2020-12-09 13:15:35 +0800

    init transport server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-kratos/kratos/v2"
+	grpctransport "github.com/go-kratos/kratos/v2/transport/grpc"
+	httptransport "github.com/go-kratos/kratos/v2/transport/http"
+	"github.com/go-kratos/service-layout/internal/service"
+
+	"google.golang.org/grpc/examples/helloworld/helloworld"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	log.Printf("version: %s\n", Version)
+
+	// transport server
+	httpSrv := httptransport.NewServer(httptransport.WithAddress(":8000"))
+	grpcSrv := grpctransport.NewServer(grpctransport.WithAddress(":9000"))
+
+	// register service
+	gs := service.NewGreeterService()
+	helloworld.RegisterGreeterServer(grpcSrv, gs)
+	httpSrv.Handler = newHTTPHandler(gs)
+
+	// application lifecycle
+	app := kratos.New()
+	app.Append(kratos.Hook{OnStart: httpSrv.Start, OnStop: httpSrv.Stop})
+	app.Append(kratos.Hook{OnStart: grpcSrv.Start, OnStop: grpcSrv.Stop})
+
+	// start and wait for stop signal
+	if err := app.Run(); err != nil {
+		log.Printf("app failed: %v\n", err)
+	}
+}
+
+func newHTTPHandler(gs *service.GreeterService) http.Handler {
+	m := http.NewServeMux()
+	m.HandleFunc("/helloworld", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "helloworld")
+	})
+	return m
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/go-kratos/service-layout
 
 go 1.15
+
+require (
+	github.com/go-kratos/kratos/v2 v2.0.0-20201208162324-68fd918dee31
+	github.com/golang/protobuf v1.4.3 // indirect
+	google.golang.org/grpc/examples v0.0.0-20201209011439-fd32f6a4fefe
+)
