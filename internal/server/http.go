3b3f3a66a30c661f100fe1530856e9516d15968d
Windfarer <windfarer@gmail.com>
2021-02-18 11:11:32 +0800

    Update http.go

--- internal/server/http.go
+++ internal/server/http.go
@@ -10,12 +10,6 @@ import (
 	"github.com/go-kratos/kratos/v2/transport/http"
 )
 
-// HTTPConfig is server config.
-type HTTPConfig struct {
-	Network string `json:"network"`
-	Address string `json:"address"`
-}
-
 // NewHTTPServer new a HTTP server.
 func NewHTTPServer(c *conf.Server) *http.Server {
 	var opts = []http.ServerOption{
