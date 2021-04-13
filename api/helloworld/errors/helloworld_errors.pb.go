d4d18b070a53c2bf7b7ef670448a4bee93493237
chenzhihui <zhihui_chen@foxmail.com>
2021-04-13 11:12:41 +0800

    add Makefiles

--- api/helloworld/errors/helloworld_errors.pb.go
+++ api/helloworld/errors/helloworld_errors.pb.go
@@ -9,3 +9,11 @@ import (
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the kratos package it is being compiled against.
 const _ = errors.SupportPackageIsVersion1
+
+const (
+	Errors_MissingName = "Helloworld_MissingName"
+)
+
+func IsMissingName(err error) bool {
+	return errors.Reason(err) == Errors_MissingName
+}
