139f828f762c7112822f287334fcf15df96ed732
包子 <baozhecheng@foxmail.com>
2021-06-09 10:48:21 +0800

    fix errors bug

--- internal/service/greeter.go
+++ internal/service/greeter.go
@@ -5,7 +5,6 @@ import (
 
 	v1 "github.com/go-kratos/kratos-layout/api/helloworld/v1"
 	"github.com/go-kratos/kratos-layout/internal/biz"
-	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 )
 
@@ -26,7 +25,7 @@ func NewGreeterService(uc *biz.GreeterUsecase, logger log.Logger) *GreeterServic
 func (s *GreeterService) SayHello(ctx context.Context, in *v1.HelloRequest) (*v1.HelloReply, error) {
 	s.log.Infof("SayHello Received: %v", in.GetName())
 	if in.GetName() == "error" {
-		return nil, errors.NotFound(v1.ErrorReason_USER_NOT_FOUND.String(), in.GetName())
+		return nil, v1.ErrorUserNotFound("%s",in.GetName())
 	}
 	return &v1.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
