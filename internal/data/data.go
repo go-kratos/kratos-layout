bf48791a379ef373ef8ac9c295ead9a9767005b6
Windfarer <windfarer@gmail.com>
2021-07-21 10:19:52 +0800

    use helper to print log to avoid KEYVALS UNPAIRED (#47)

--- internal/data/data.go
+++ internal/data/data.go
@@ -17,7 +17,7 @@ type Data struct {
 // NewData .
 func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 	cleanup := func() {
-		logger.Log(log.LevelInfo, "closing the data resources")
+		log.NewHelper(logger).Info("closing the data resources")
 	}
 	return &Data{}, cleanup, nil
 }
