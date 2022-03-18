2b41e974270808f7e8f56f023f9d0e7175400be6
chenzhihui <zhihui_chen@foxmail.com>
2022-03-18 13:57:16 +0800

    fix impl

--- internal/data/greeter.go
+++ internal/data/greeter.go
@@ -20,10 +20,22 @@ func NewGreeterRepo(data *Data, logger log.Logger) biz.GreeterRepo {
 	}
 }
 
-func (r *greeterRepo) CreateGreeter(ctx context.Context, g *biz.Greeter) (*biz.Greeter, error) {
+func (r *greeterRepo) Save(ctx context.Context, g *biz.Greeter) (*biz.Greeter, error) {
 	return g, nil
 }
 
-func (r *greeterRepo) UpdateGreeter(ctx context.Context, g *biz.Greeter) (*biz.Greeter, error) {
+func (r *greeterRepo) Update(ctx context.Context, g *biz.Greeter) (*biz.Greeter, error) {
+	return g, nil
+}
+
+func (r *greeterRepo) FindByID(context.Context, int64) (*biz.Greeter, error) {
+	return nil, nil
+}
+
+func (r *greeterRepo) ListByHello(context.Context, string) ([]*biz.Greeter, error) {
+	return nil, nil
+}
+
+func (r *greeterRepo) ListAll(context.Context) ([]*biz.Greeter, error) {
 	return nil, nil
 }
