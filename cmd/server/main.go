99679870e459765c4e58fe8a6fcdf0f2886835e0
zackZhang <zhangkaiamm@gmail.com>
2024-06-30 10:22:23 +0800

    optimize: set default config path
    Signed-off-by: zackZhang <zhangkaiamm@gmail.com>

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,7 +30,7 @@ var (
 )
 
 func init() {
-	flag.StringVar(&flagconf, "conf", "../../configs", "config path, eg: -conf config.yaml")
+	flag.StringVar(&flagconf, "conf", "./configs", "config path, eg: -conf config.yaml")
 }
 
 func newApp(logger log.Logger, gs *grpc.Server, hs *http.Server) *kratos.App {
